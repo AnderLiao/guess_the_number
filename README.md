81c7498e6d73bb7e20209b10c2451df0ac0b8944
Ander Liao <andercola@gmail.com>
2020-04-21 02:57:44 +0800

    Revised README.md and 1st draft of guessing number game

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # guess_the_number
 - Create a random number from 1 to 100
 - User can guess a number between the random number
-- Print our "You win" if user guessed the right number
+- Print out "You win" if user guessed the right number
+- Print out "Higher" or "Lower" if the number is wrong
 - Don't stop until the use guess the right one
--- guess_the_number.py
+++ guess_the_number.py
@@ -1,1 +1,19 @@
-guess_the_number.py
+# - Create a random number from 1 to 100
+# - User can guess a number between the random number
+# - Print out "You win" if user guessed the right number
+# - Print out "Higher" or "Lower" if the number is wrong
+# - Don't stop until the use guess the right one
+
+import random
+
+roll = random.randint(1,100)
+
+while True:
+	user_input = int(input('Guess a number between 1 to 100: '))
+	if user_input > roll:
+		print('Lower, try again!')
+	elif user_input < roll:
+		print('Higher, try again!')
+	else:
+		print('Bingo!')
+		break
